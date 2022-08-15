9f4ce6baa3aa40635d5e9538a85dc1c70b58943d
unknown <nzrkodi@gmail.com>
2022-08-15 15:11:47 +0800

    tampilan onepage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\User\Dashboardcontroller as UserDashboardcontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/user', [UserDashboardcontroller::class, 'index']);
